d9f48f938d3e4d26671db387bbbbb7105d2f3b30
Durga Lovababu Padala <durgalovababupadala@MacBook-Air.local>
2024-12-01 19:27:26 +0530

    Added input/output and conditional operations examples

---
+++ 13-InputOutputFuns/main.py
@@ -0,0 +1,22 @@
+# Input and Output Function.
+
+# Print is output function.
+print("Hello World.") 
+
+# Input function is a built in fuction.
+
+print("Enter your Name: ")
+ans = input()
+
+print("Your Name is : " + ans)
+
+print("Enter your Age.")
+age = input()
+
+print(f"You are {age} year old.")
+
+address = input("Where do you live in?\n")
+
+print("You lives in a city called, " + address)
+
+
---
+++ 14-ConditionalOps/main.py
@@ -0,0 +1,33 @@
+# Usage of If else else-if
+
+is_cold = True
+is_rain = False
+#if it is cold carry jacket, else dont carry.
+if is_cold:
+    print("Hello customer please carry your jacket")
+else:
+    print("It is going to hot, dont carry jackets to reduce the weight.")
+
+#if it is cold carry jacket, if it is rain carry umbrella, else with fee hands.
+
+if is_cold:
+    print("Carry jacket")
+elif is_rain:
+    print("Carry umberella")
+else:
+    print("come with free hands")
+
+# Nested Ifs.
+print("*" * 10 + "Nested Ifs." + "*" * 10)
+# if it is cold and you are above 60 please carry jackets and mufflers
+# else free hands
+age = 65
+if is_cold:
+    if age > 60:
+        print("carry jackets and muflers")
+    else:
+        print("only carry jackets")
+else:
+    print("come with free hands.")
+
+
